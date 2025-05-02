b614df2b16a8e343e0742616c3de54d274442ffb
Maryam <marium6343@gmail.com>
2025-05-02 14:39:42 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import './App.css';
+import Navigation from "./assets/Components/Navigation";
+import Inner from "./assets/Components/inner";
+
+
+const  APP = ()=>{
+  return(
+    <>
+    <Navigation/>
+    <Inner/>
+    
+    </>
+   
+  )
+
+};
+export default APP;
---
+++ src/assets/Components/Navigation.jsx
@@ -0,0 +1,24 @@
+const  Navigation = ()=>{
+    return(
+      <> 
+      <div>
+        <nav className="container">
+          <div className="logo">
+            <img src="images/brand_logo.png"></img></div>
+  
+            <ul>
+              <li>MENU</li>
+              <li>LOCATION</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+            </ul>
+            <button>Login</button>
+  
+          
+        </nav>
+      </div>
+       </>
+    )
+  
+  };
+  export default Navigation;
